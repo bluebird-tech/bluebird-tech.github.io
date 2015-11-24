f895e45d71428d16efceed89b178fa09cbe2306e
alicejo <alice.vasco@boost-project.com>
2015-11-24 12:27:02 +0100

    base bluebird site

--- assets/javascript/background.js
+++ assets/javascript/background.js
@@ -7,18 +7,18 @@ function changeBackgroundVideo() {
   var source = document.createElement('source');
 
 
-  if (7 <= currentTime <= 20) {
+  if (7 < currentTime && currentTime < 17) {
     video.setAttribute('poster', 'assets/images/cloudspic.jpg')
-    source.setAttribute('src', 'assets/videos/clouds.mp4');
+    source.setAttribute('src', 'assets/videos/clouds2.mp4');
   }
 
   else {
     source.setAttribute('src', 'assets/videos/aurora2.mp4');
-  }
+  };
 
   video.appendChild(source);
   video.load();
+  console.log(currentTime);
 };
 
 changeBackgroundVideo();
-console.log("Hello");
